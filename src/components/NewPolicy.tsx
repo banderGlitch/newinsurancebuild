202e2803166516708fd59a8aaba65cba29fe8e8f
banderGlitch <nernaykumar98@gmail.com>
2024-09-05 16:56:18 +0530

    resolved unresolved variable

--- src/components/NewPolicy.tsx
+++ src/components/NewPolicy.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import VehicleDetails from '../MainApp/NewPolicyComp/VehicleDetails';
 import CoverageInput from '../MainApp/NewPolicyComp/CoverageInput';
-import InsurerTable from '../MainApp/NewPolicyComp/InsurerTable';
 import InsuranceIcon from './InsuranceIcon';
 
 const NewPolicy: React.FC = () => {
@@ -13,21 +12,9 @@ const NewPolicy: React.FC = () => {
             <div className="w-full max-w-lg">
                 {/* Vehicle Details Section */}
                 <VehicleDetails />
-
                 {/* Coverage Input Section */}
                 <CoverageInput coverageLimit={coverageLimit} setCoverage={setCoverage} />
 
-                {/* Insurer Table */}
-                {/* <div className="flex-grow overflow-y-auto">
-                    <InsurerTable coverageLimit={coverageLimit} />
-                </div> */}
-
-                {/* Confirm & Pay Button */}
-                {/* <div className="mt-0 sticky bottom-0">
-                    <button className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">
-                        Confirm & Pay
-                    </button>
-                </div> */}
             </div>
         </div>
     );
