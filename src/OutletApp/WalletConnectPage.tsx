08aae3aaa0e1d60eed6bb9478660662297c17d9b
banderGlitch <nernaykumar98@gmail.com>
2024-09-07 15:50:33 +0530

    metamask login again

--- src/OutletApp/WalletConnectPage.tsx
+++ src/OutletApp/WalletConnectPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import InsuranceIcon from '../components/InsuranceIcon';
 import WalletComponent from './WalletConnectAuth'; // Import the WalletComponent for wallet connection
